673c1f6c2fa7f4ae102aaf45366d65d1b2bb361c
Jean Carlos Martins Ross <jeanross@MacBook-Pro-de-Jean.local>
2020-07-29 22:45:27 -0300

    Mispelling

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
 import Slider, { SliderItem } from './components/Slider';
-8
+
 function Carousel({
   ignoreFirstVideo,
   category,
